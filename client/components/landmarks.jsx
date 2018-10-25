661a5be16c4b28f161f74426917b38318a8210f9
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-25 16:02:29 -0400

    Refactor landmarks table to use <table> element
    Refactor landmarks table to use HTML <table> element for greater
    readability. The table is not yet styled and that will be
    completed on a later feature branch.

--- client/components/landmarks.jsx
+++ client/components/landmarks.jsx
@@ -11,21 +11,16 @@ class Landmarks extends React.Component {
         <h3>Nearby landmarks</h3>
         <div className="landmarks-table" style={{"width": "700px"}}>
 
-          <div className="landmarks-names" style={{"width": "500px", "float": "left"}}>
-          Hyde Park
-          </div>
-
-          <div className="landmarks-distances" style={{"width": "70px", "margin-left": "500px"}}>
-          0.3 mi
-          </div>
-
-          <div className="landmarks-names" style={{"width": "500px", "float": "left"}}>
-          Victoria & Albert Museum
-          </div>
-
-          <div className="landmarks-distances" style={{"width": "70px", "margin-left": "500px"}}>
-          0.8 mi
-          </div>
+          <table>
+            <tr>
+              <td>Hyde Park</td>
+              <td>0.3 mi</td>
+            </tr>
+            <tr>
+              <td>Victoria & Albert Museum</td>
+              <td>0.8 mi</td>
+            </tr>
+          </table>
 
         </div>
       </div>
