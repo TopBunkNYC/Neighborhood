feb6b56312842a4979f9352ff5a2044cf3f2f6d7
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-25 11:02:43 -0400

    Define database schema and set up routes to reference controllers

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -3,5 +3,5 @@ const models = require('../models/models');
 // This module will be populated with methods to fulfill requests from the client
 // It can invoke the models to do so
 module.exports = {
-
+  getListingData: (req, res) => { res.send('hello, listingdata!') }
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/main');
 
 router.get('/', (req, res) => {res.send('Hello, world')})
+router.get('/listing', (req, res) => {res.send('Hello, listing')})
+router.get('/listingdata', controller.getListingData)
 
 module.exports = router;
