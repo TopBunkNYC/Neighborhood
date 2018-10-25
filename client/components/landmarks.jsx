973bbc100f09b61a71ba22572987e0948b195c97
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-25 15:58:53 -0400

    Add in hardcoded landmarks table with two entries

--- client/components/landmarks.jsx
+++ client/components/landmarks.jsx
@@ -9,7 +9,25 @@ class Landmarks extends React.Component {
     return (
       <div id="landmarks-section">
         <h3>Nearby landmarks</h3>
-        <p>There will be a table of nearby landmarks populated here.</p>
+        <div className="landmarks-table" style={{"width": "700px"}}>
+
+          <div className="landmarks-names" style={{"width": "500px", "float": "left"}}>
+          Hyde Park
+          </div>
+
+          <div className="landmarks-distances" style={{"width": "70px", "margin-left": "500px"}}>
+          0.3 mi
+          </div>
+
+          <div className="landmarks-names" style={{"width": "500px", "float": "left"}}>
+          Victoria & Albert Museum
+          </div>
+
+          <div className="landmarks-distances" style={{"width": "70px", "margin-left": "500px"}}>
+          0.8 mi
+          </div>
+
+        </div>
       </div>
     )
   }
