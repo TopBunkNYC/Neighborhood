7e23782b9395c7ccf891ef52d9608192226e1272
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-26 11:00:23 -0400

    Render map using listing location passed down as props

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import MapContainer from './map-container.jsx'
+let GoogleMapsAPIKEY =  require('../../config.js');
 
 class Map extends React.Component {
   constructor(props) {
@@ -8,7 +10,14 @@ class Map extends React.Component {
   render() {
     return (
       <div id="map-section">
-        <h4 style={{"color": "gray"}}>MAP GOES HERE</h4>
+        <MapContainer 
+          isMarkerShown 
+          listingLocation={this.props.listingLocation} 
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${GoogleMapsAPIKEY}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />} 
+        />
         <div>Exact location information is provided after a booking is confirmed.</div>
       </div>
     )
--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
 import Neighborhood from './components/neighb-index.jsx';
+import MapContainer from './components/map-container.jsx'
 
 window.Neighborhood = Neighborhood;
+window.MapContainer = MapContainer;
