eedd30d528309e4dfee6b39a4fb351add9ad7bb6
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-31 19:20:28 -0400

    Add window.location.origin to outgoing axios requests

--- client/components/neighb-index.jsx
+++ client/components/neighb-index.jsx
@@ -27,11 +27,12 @@ export default class Neighborhood extends React.Component {
 
   componentDidMount() {
     let queryString = window.location.search;
+    // console.log('window.location looks like', window.location)
     let listingId = (queryString.slice(-3) * 1)
     this.setState({listingId: listingId})
 
     // Get listing data
-    axios.get(`/listingdata?id=${listingId}`)
+    axios.get(`${window.location.origin}/listingdata?id=${listingId}`)
 
     // axios.get('http://ec2-3-16-89-66.us-east-2.compute.amazonaws.com/listingdata?id=97')
     .then(({data}) => {
@@ -51,7 +52,7 @@ export default class Neighborhood extends React.Component {
     // Get neighborhood data
     .then(() => {
       let neighbId = this.state.neighborhoodId;
-      axios.get(`/neighborhooddata?id=${neighbId}`)
+      axios.get(`${window.location.origin}/neighborhooddata?id=${neighbId}`)
       .then(({data}) => {
         this.setState({
           neighbName: data[0].neighbName,
@@ -66,7 +67,7 @@ export default class Neighborhood extends React.Component {
 
     // Get landmark data for five nearest landmarks to this location
     .then(() => {
-      axios.get(`/landmarkdata?listingLat=${this.state.listingLat}&listingLong=${this.state.listingLong}`)
+      axios.get(`${window.location.origin}/landmarkdata?listingLat=${this.state.listingLat}&listingLong=${this.state.listingLong}`)
       .then(({data}) => {
         this.setState({
           nearbyLandmarks: data,
