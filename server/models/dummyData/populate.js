5845b936824eee4d7ec7f787c6e4ed8b3d38febe
Work <work@casey395-75181.local>
2018-11-10 15:04:42 -0500

    Put back necessary imports

--- server/models/dummyData/populate.js
+++ server/models/dummyData/populate.js
@@ -9,8 +9,44 @@ const Neighborhood = models.neighborhoodSchema;
 const Landmark = models.landmarkSchema;
 const db = require("../../../database/index.js");
 const { performance } = require("perf_hooks");
+const faker = require("faker");
+const randomPointsOnPolygon = require("random-points-on-polygon");
+const turf = require("turf");
 
 var t0 = performance.now();
+
+
+var polygon = turf.polygon([
+  [
+    [-0.2451, 51.5194],
+    [-0.196, 51.5485],
+    [-0.104, 51.5485],
+    [-0.07, 51.5331],
+    [-0.0632, 51.5132],
+    [-0.0748, 51.5089],
+    [-0.1095, 51.5128],
+    [-0.1218, 51.5108],
+    [-0.1281, 51.4972],
+    [-0.1428, 51.4867],
+    [-0.1665, 51.4869],
+    [-0.185, 51.4811],
+    [-0.2451, 51.5194]
+  ]
+]);
+
+let numberOfPoints = 100;
+var points = randomPointsOnPolygon(numberOfPoints, polygon);
+
+let listingsCoords = [];
+
+for (let i = 0; i < points.length; i++) {
+  // reverse order to lat-long instead long-lat
+  let latLong = [
+    points[i].geometry.coordinates[1],
+    points[i].geometry.coordinates[0]
+  ];
+  listingsCoords.push(latLong);
+}
 
 // Redis Listings load
 (async () => {
@@ -38,7 +74,7 @@ var t0 = performance.now();
   console.log("Redis load took " + (t1 - t0) / 1000 + " seconds.");
 })();
 
-//Postgres Listings load
+// Postgres Listings load
 db.query(
   `COPY listings ("hostFirstName","listingLat","listingLong","neighbId","neighbDesc","gettingAroundDesc","createdAt","updatedAt") FROM '/Users/work/Desktop/sdc/Neighborhood/server/models/dummyData/data.csv' DELIMITER ',' CSV HEADER;`,
   { model: Listing }
