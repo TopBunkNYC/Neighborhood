62efd833afcce5e8bce0c28311613ae627fcc078
kchen395 <kchen395@gmail.com>
2018-12-06 10:29:28 -0500

    Clean up code

--- database/index.js
+++ database/index.js
@@ -1,28 +1,24 @@
-const Sequelize = require('sequelize');
-const password = require('../config.js').DB_PASSWORD;
+const Sequelize = require("sequelize");
+const password = require("../config.js").DB_PASSWORD;
 
-const db  = new Sequelize('neighborhood', 'postgres', password, {
-	host: "3.16.206.192",
-	dialect: 'postgres',
-	pool: {
-		max: 1000,
-		min: 0,
-		idle: 10000,
-		acquire: 10000
-	},
-	logging: false		
+const db = new Sequelize("neighborhood", "postgres", password, {
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 1000,
+    min: 0,
+    idle: 10000,
+    acquire: 10000
+  },
+  logging: false
 });
-
 
 db.authenticate()
   .then(() => {
-    console.log('PostgreSQL connection has been established successfully.');
+    console.log("PostgreSQL connection has been established successfully.");
   })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
   });
 
 module.exports = db;
-
-
-
